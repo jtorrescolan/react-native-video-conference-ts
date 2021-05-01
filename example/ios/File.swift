727a8d7e7a40d84ac8fdbf5d1c601cda907dda5f
jtorrescolan <jtorrescolan@gmail.com>
2021-04-30 20:00:50 -0500

    remove example folder and add checkbox component

--- example/ios/File.swift
+++
@@ -1,6 +0,0 @@
-//
-//  File.swift
-//  VideoConferenceTsExample
-//
-
-import Foundation
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,16 @@
-export default {
-  multiply(a: number, b: number) {
-    return Promise.resolve(a * b);
-  },
+import * as React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+import CheckBox from '@react-native-community/checkbox';
+
+const Demo: React.FC<any> = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hola mundo</Text>
+        <CheckBox disabled={false} value={true} onValueChange={() => true} />
+      </View>
+    </SafeAreaView>
+  );
 };
+
+export { Demo };
