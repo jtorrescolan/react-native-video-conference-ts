281eb22b00e830933b1fcea0f0b33103c1d18808
jtorrescolan <jtorrescolan@gmail.com>
2021-05-02 16:39:37 -0500

    add .babelrc

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,3 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'babel-plugin-root-import',
-      {
-        rootPathSuffix: './',
-        rootPathPrefix: '~/',
-      },
-    ],
-    /* [
-      'module-resolver',
-      {
-        root: ['./'],
-        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
-        alias: {
-          '@components': './src/components',
-        },
-      },
-    ], */
-  ],
 };
