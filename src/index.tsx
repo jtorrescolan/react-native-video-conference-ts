fbef922ac655ec72106bd1ff283dcca215d8e29b
jtorrescolan <jtorrescolan@gmail.com>
2021-05-02 20:27:44 -0500

    remove src files

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,20 @@
 import * as React from 'react';
-import { View, Text, SafeAreaView } from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 import CheckBox from '@react-native-community/checkbox';
 import { AddComponent } from '@components/Add';
 
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+});
+
 const Demo: React.FC<any> = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={style.container}>
       <View>
-        <Text>Hola mundo</Text>
+        <Text>Hello world</Text>
         <CheckBox disabled={false} value={true} onValueChange={() => true} />
         <AddComponent />
       </View>
